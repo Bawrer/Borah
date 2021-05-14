73c041c40988fcb62181d674ad98ee2cb9211c68
Bawrer <84140652+Bawrer@users.noreply.github.com>
2021-05-14 13:44:59 +0200

    Create Borah.java

---
+++ Borah.java
@@ -0,0 +1,14 @@
+//Programming is fun
+
+import javax.swing.*;
+
+Public class Borah
+{
+public static void main(string [] args)
+{
+JoptioPane.showMessageDialog(null," programming is fun");
+System.Exit(0);
+}
+
+
+}
